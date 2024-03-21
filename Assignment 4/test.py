bd3ddd0f15f97a5ff15693ec4c89ec567f7d307a
Alok Dhar Dubey <ialok00001@gmail.com>
2024-03-22 01:17:29 +0530

    Updated test.py and coverage.txt

--- Assignment 4/test.py
+++ Assignment 4/test.py
@@ -4,13 +4,15 @@ import time
 
 def test_docker():
     subprocess.Popen(['docker', 'run', '-d', '-p', '5000:5000', 'flask-app'])
-    time.sleep(2)  # Allow time for container to start
+    time.sleep(2)
     
     response = requests.post('http://localhost:5000', data={'text': 'Sample text'})
-    
     assert response.status_code == 200
-    
+
     subprocess.Popen(['docker', 'stop', '$(docker ps -q)'])
+    subprocess.Popen(['docker', 'rm', '$(docker ps -a -q)'])
+    subprocess.Popen(['docker', 'rmi', 'flask-app'])
+    subprocess.Popen(['docker', 'image', 'prune', '-f'])
 
 if __name__ == "__main__":
     test_docker()
